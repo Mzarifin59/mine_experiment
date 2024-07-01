a9aaaf0113907280fc28f5898bbcac6beb77471e
unknown <arifinrifky326@gmail>
2024-07-01 13:30:55 +0700

    Penambahan fitur HTTP

--- index.js
+++ index.js
@@ -5,3 +5,19 @@
 // body.append(app)
 
 console.log('Halo Dunia');
+
+// // Contoh Membuat HTTP Server dari web node js
+// const http = require('http');
+
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello, World!\n');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
